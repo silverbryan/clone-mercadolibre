7944d0528669d90609ae3d904ba0bcc96f870520
silverbryan <soporte.g4a@gmail.com>
2020-11-03 14:55:37 -0300

    update search route

--- api/src/routes/search.js
+++ api/src/routes/search.js
@@ -1,14 +1,32 @@
-// require('dotenv').config();
+require('dotenv').config();
 const axios = require('axios');
 const server = require("express").Router();
 
-// const { PATH_BASE, LIMIT_RESULTS } = process.env;
+const { PATH_BASE } = process.env;
 
 server.get('/', (req, res) => {
     const searchTerm = req.query.q;
-    axios.get(PATH_BASE + '/sites/MLA/search?q=' + searchTerm)
+    let paging = req.query.page;
+    if (!paging) paging = 1;
+
+    axios.get(PATH_BASE + '/sites/MLA/search?q=' + searchTerm + '&limit=30&offset=' + page)
         .then(response => {
-            res.status(200).json({ res: response.data.results });
+            var data = response.data.results;
+            var arrResponse = [];
+            data.map(result => {
+                arrResponse.push(
+                    {
+                        id: result.id,
+                        title: result.title,
+                        price: result.price,
+                        currency_id: result.currency_id,
+                        available_quantity: result.available_quantity,
+                        thumbnail: result.thumbnail,
+                        condition: result.condition,
+                    }
+                )
+            });
+            res.status(200).json({ status: 'OK', results: arrResponse });
         })
         .catch(err => res.status(500).json({ message: 'unknown error', error: err }));
 });
