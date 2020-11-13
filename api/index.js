413b10fbf1d17de2cd1f078d3e0d20b48a382fa8
silverbryan <soporte.g4a@gmail.com>
2020-11-13 19:00:48 -0300

    update ports

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const server = require("./src/app.js");
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
     console.log("%s listening at " + PORT);
