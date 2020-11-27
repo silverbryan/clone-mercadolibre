eabf182cf888429267405babcccdb03686be1d38
silverbryan <soporte.g4a@gmail.com>
2020-11-27 15:37:50 -0300

    solved conflicts

--- api/src/app.js
+++ api/src/app.js
@@ -13,7 +13,14 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use(cors());
-server.use(express.static(path.join(__dirname, '../../client/build')));
+
+const buildDir = path.join(__dirname, '../../client/build')
+
+server.use(express.static(buildDir));
+
+server.get('*', (req, res) => {
+    res.sendFile(path.join(buildDir, 'index.html'));
+})
 
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Credentials', 'true');
